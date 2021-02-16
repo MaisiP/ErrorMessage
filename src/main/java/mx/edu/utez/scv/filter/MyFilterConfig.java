0d7b0b4cb113dc57154ebd6d26d1810eb958dbda
monica.maisi <monica.perez.0223@gmail.com>
2021-02-15 22:30:36 -0600

    Revert "Filter v(infinito)"
    This reverts commit 6065d79d668de3efa13ae901ac631b78b2d89f7d.

--- src/main/java/mx/edu/utez/scv/filter/MyFilterConfig.java
+++ src/main/java/mx/edu/utez/scv/filter/MyFilterConfig.java
@@ -1,7 +1,5 @@
 package mx.edu.utez.scv.filter;
 
-import java.util.Arrays;
-
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +14,8 @@ public class MyFilterConfig {
 		
 		
 		registrationBean.setFilter(new MyFilterInterface());
-		registrationBean.setUrlPatterns(Arrays.asList("/app/mascota/index*","/app/mascota/save*","/app/mascota/edit/[0-999]*","/app/mascota/update*","/app/mascota/delete*","/app/mascota/create*"));
+		registrationBean.addUrlPatterns("/app/mascota/save*","/app/mascota/edit/[0-999]*","/app/mascota/update*","/app/mascota/delete*","/app/mascota/create*");
+		
 		return registrationBean;
 	}
 
